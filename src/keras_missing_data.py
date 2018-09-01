a5350c5ee2c5f21657bfaec6a4c7f34d68926e33
Gereon Kaiping <anaphory@yahoo.de>
2018-09-01 14:48:23 +0200

    Find a way to deal with missing data

--- src/keras_missing_data.py
+++ src/keras_missing_data.py
@@ -14,6 +14,9 @@ import numpy
 
 def loss_0_where_nan(loss_function, msg=""):
     def filtered_loss_function(y_true, y_pred):
+        print(hidden_layer.get_weights())
+        print(min_pred_layer.get_weights())
+        print(max_min_pred_layer.get_weights())
         with_nans = loss_function(y_true, y_pred)
         nans = tf.is_nan(with_nans)
         filtered = tf.where(nans, tf.zeros_like(with_nans), with_nans)
@@ -34,12 +37,18 @@ max_min_pred = max_min_pred_layer(hidden)
 
 model = Model(inputs=[input],
               outputs=[min_pred, max_min_pred])
+truncated_model = Model(inputs=[input],
+                        outputs=[min_pred])
 
 model.compile(
     optimizer=Adadelta(),
     loss=[loss_0_where_nan(mean_squared_error, "aux: "),
           loss_0_where_nan(mean_squared_error, "main: ")],
     loss_weights=[0.2, 1.0])
+
+truncated_model.compile(
+    optimizer=Adadelta(),
+    loss=[loss_0_where_nan(mean_squared_error, "aux: ")])
 
 def random_values(n, missing=False):
     for i in range(n):
@@ -50,7 +59,11 @@ def random_values(n, missing=False):
         else:
             _max_min = numpy.maximum(_min, x[..., 2]).reshape((-1, 1))
         # print(x, numpy.array(_min).reshape((-1, 1)), numpy.array(_max_min), sep="\n", end="\n\n")
-        yield x, [numpy.array(_min).reshape((-1, 1)), numpy.array(_max_min)]
+
+        if missing:
+            yield x, numpy.array(_min).reshape((-1, 1))
+        else:
+            yield x, [numpy.array(_min).reshape((-1, 1)), numpy.array(_max_min)]
 
 model.fit_generator(random_values(2, False),
                     steps_per_epoch=2,
@@ -59,9 +72,10 @@ print("With missing")
 print(hidden_layer.get_weights())
 print(min_pred_layer.get_weights())
 print(max_min_pred_layer.get_weights())
-history = model.fit_generator(random_values(1, True),
-                              steps_per_epoch=1,
-                    verbose=False)
+history = truncated_model.fit_generator(
+    random_values(1, True),
+    steps_per_epoch=1,
+    verbose=False)
 print(hidden_layer.get_weights())
 print(min_pred_layer.get_weights())
 print(max_min_pred_layer.get_weights())
