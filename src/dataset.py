f0c1cdee7bb5a375acfb2a5665d91d9f49cc69bb
Gereon Kaiping <anaphory@yahoo.de>
2018-10-13 01:17:40 +0200

    Slicker dataset implementation

--- src/dataset.py
+++ src/dataset.py
@@ -1,102 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env
 
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-# Obtained under the Apache License, Version 2.0 (the "License");
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# and modified.
-#
-# Copyright 2018 Gereon Kaiping.
+"""Various sound datasets"""
 
-import itertools
+import numpy
+import bisect
 from pathlib import Path
 
-from keras.utils import Sequence
-
-import bisect
-import numpy
 import tensorflow as tf
-
-from phonetic_features import N_FEATURES, feature_vector_of_sound, xsampa
-import read_textgrid
-from hparams import hparams as p
-prediction_length_windows = -(-p["prediction_time_at_least_ms"] //
-                              p["frame_shift_ms"])
-# Negative number magic to round to the next int
-
-from spectrogram_to_sound import stft
+from keras.utils import Sequence
 from keras.preprocessing.sequence import pad_sequences
 
-this = Path(__file__)
-DATA_PATH = this.parent.parent / "data"
+import read_textgrid
+
+from hparams import hparams
+
+try:
+    this = Path(__file__).absolute()
+except NameError:
+    this = Path("__file__").absolute()
+DATA_PATH = this.parent.parent / "data" / "selection"
 
 
-def list_wavfiles():
-    for file in itertools.chain(DATA_PATH.glob("**/*.wav")):
-        yield file.absolute()
+SEGMENTS = [
+    None, '#H', '*', '2:', '2:6', '6', '9', '@', 'ASpause', 'C', 'E',
+    'E6', 'E:', 'E:6', 'H#', 'I', 'I6', 'N', 'O', 'O6', 'OY', 'Q', 'S', 'U',
+    'U6', 'Y', 'a', 'a6', 'a:', 'a:6', 'aI', 'aU', 'b', 'd', 'e:', 'e:6', 'f',
+    'g', 'h', 'i:', 'i:6', 'j', 'k', 'l', 'm', 'n', 'o:', 'o:6', 'p', 'r', 's',
+    't', 'u:', 'u:6', 'v', 'x', 'y:', 'y:6', 'z']
+
+def stft_parameters():
+    n_fft = (hparams["n_spectrogram"] - 1) * 2
+    hop_length = int(hparams["frame_shift_ms"] / 1000 * hparams["sample_rate"])
+    win_length = int(hparams["frame_length_ms"] / 1000 * hparams["sample_rate"])
+    return n_fft, hop_length, win_length
 
 
-def wavfile_with_textgrid(files=list(list_wavfiles()), shuffle=True):
-    if shuffle:
-        numpy.random.shuffle(files)
-    for file in files:
-        try:
-            with file.with_suffix(".TextGrid").open() as tr:
-                textgrid = tr.read()
-        except UnicodeDecodeError:
-            tr.close()
-            with file.with_suffix(".TextGrid").open(encoding="utf-16") as tr:
-                textgrid = tr.read()
-        except FileNotFoundError:
-            textgrid = None
-            feature_matrix = None
-
-        path, format = str(file), file.suffix[1:]
-        audio = read_audio(path, format)
-
-        if textgrid:
-            textgrid = read_textgrid.TextGrid(textgrid)
-
-            phonetics = textgrid.tiers[0]
-            if phonetics.nameid == "Phonetic":
-                # Assume XSAMPA
-                transform = xsampa
-            elif phonetics.nameid == "PhoneticIPA":
-                transform = None
-            elif phonetics.nameid == "MAU": # WebMAUS output
-                transform = None
-            else:
-                raise ValueError("Unexpected tier found in file {:}: {:}".format(
-                    file, phonetics.nameid))
-
-            windows_per_second = 1000 / p["frame_shift_ms"]
-
-            feature_matrix = numpy.zeros(
-                (int(float(phonetics.simple_transcript[-1][1]) *
-                     windows_per_second -
-                     p["frame_length_ms"] / p["frame_shift_ms"]) +
-                 1,
-                N_FEATURES),
-                dtype=bool)
-            for start, end, segment in phonetics.simple_transcript:
-                start = float(start)
-                end = float(end)
-                if transform:
-                    segment = transform(segment)
-                window_features = feature_vector_of_sound(segment)
-                for window in range(int(start * windows_per_second),
-                                    int(end * windows_per_second)):
-                    try:
-                        feature_matrix[window] = window_features
-                    except IndexError:
-                        continue
-
-        yield audio, feature_matrix
-
-
-def audio_path_and_type(files=list(list_wavfiles())):
-    for path in files:
-        yield str(path), path.suffix[1:]
+def stft(signals):
+    n_fft, hop_length, win_length = stft_parameters()
+    return tf.contrib.signal.stft(signals, win_length, hop_length,
+                                  n_fft, pad_end=False)
 
 
 g = tf.Graph()
@@ -105,29 +47,24 @@ with g.as_default():
         tf.contrib.ffmpeg.decode_audio(
             tf.read_file(tf.placeholder(tf.string, name="filename")),
             file_format=tf.placeholder(tf.string, name="format"),
-            samples_per_second=p["sample_rate"],
+            samples_per_second=hparams["sample_rate"],
             channel_count=1))
 
     log_mag_spectrogram = tf.log(tf.abs(stft(waveform) + 1e-8))
 
 
+
 def read_audio(filename, format):
     with tf.Session(graph=g) as sess:
-        return sess.run(waveform, feed_dict={
+        return sess.run(log_mag_spectrogram, feed_dict={
             'filename:0': str(filename),
             'format:0': format})
 
 
-def spectrogram(wave):
-    with tf.Session(graph=g) as sess:
-        return sess.run(log_mag_spectrogram, feed_dict={
-            waveform: wave})
-
-
-class AudiofileSequence(Sequence):
+class TimeAlignmentSequence(Sequence):
     def __init__(self, batch_size=10, files=None):
         if files is None:
-            files = list_wavfiles()
+            files = DATA_PATH.glob("*.TextGrid")
         self.batch_size = batch_size
         sizes = []
         self.files = []
@@ -135,8 +72,7 @@ class AudiofileSequence(Sequence):
             try:
                 sg = numpy.load(file.with_suffix(".npy").open("rb"))
             except (OSError, FileNotFoundError):
-                waveform = read_audio(file, 'wav')
-                sg = spectrogram(waveform)
+                sg = read_audio(file.with_suffix(".wav"), 'wav')
                 numpy.save(file.with_suffix(".npy").open("wb"), sg)
             i = bisect.bisect(sizes, len(sg))
             sizes.insert(i, len(sg))
@@ -149,44 +85,28 @@ class AudiofileSequence(Sequence):
         return -(-len(self.files) // self.batch_size)
 
     def __getitem__(self, index):
-        spectrograms = [
-            numpy.load(file.with_suffix(".npy").open("rb"))
-            for file in self.files[index * self.batch_size: (index + 1) * self.batch_size]]
-        return pad_sequences(spectrograms,
-                             dtype=numpy.float32,
-                             value=numpy.log(1e-8))
-
-
-class ShiftedSpectrogramSequence(AudiofileSequence):
-    def __getitem__(self, index):
-        raw = super().__getitem__(index)
-        return (
-            raw[:, prediction_length_windows:-prediction_length_windows],
-            [raw[:, :-2 * prediction_length_windows],
-             raw[:, 2 * prediction_length_windows:]])
-
-
-class SpectrogramFeaturesSequence(AudiofileSequence):
-    def __init__(self, batch_size=10, files=list_wavfiles()):
-        files = [file
-                 for file in files
-                 if file.with_suffix(".TextGrid").exists()]
-        super().__init__(batch_size=batch_size, files=files)
-
-    def __getitem__(self, index):
         try:
-            spectrograms = super().__getitem__(index)
+            sgs = [
+                numpy.load(file.with_suffix(".npy").open("rb"))
+                for file in self.files[
+                        index * self.batch_size: (index + 1) * self.batch_size]]
+            spectrograms = numpy.zeros(
+                    (len(sgs),
+                     len(sgs[-1]),
+                     len(sgs[-1][-1])))
+            for i, sg in enumerate(sgs):
+                spectrograms[i][:len(sg)] = sg
+
             files = self.files[index * self.batch_size:
                         	   (index + 1) * self.batch_size]
-            feature_values = [
-                numpy.zeros(
-                    (spectrograms.shape[0], spectrograms.shape[1], 2))
-                for feature_id in range(N_FEATURES)]
+            feature_values = numpy.zeros(
+                    (len(spectrograms),
+                     len(spectrograms[-1]),
+                     len(SEGMENTS)))
+
             for i, file in enumerate(files):
-                for f, feature in enumerate(self.features_from_textgrid(
-                        file, spectrograms.shape[1]).T):
-                    feature_values[f][i, :, 0] = 1 - feature
-                    feature_values[f][i, :, 1] = feature
+                feature_values[i] = self.features_from_textgrid(
+                        file, spectrograms.shape[1])
         except Exception as e:
             # Keras eats all errors, make sure to at least see them in the console
             print(e, end="\n\n")
@@ -199,33 +119,16 @@ class SpectrogramFeaturesSequence(AudiofileSequence):
             textgrid = read_textgrid.TextGrid(tr.read())
 
         phonetics = textgrid.tiers[0]
-        if phonetics.nameid == "Phonetic":
-            # Assume XSAMPA
-            transform = xsampa
-        elif phonetics.nameid == "PhoneticIPA":
-            transform = None
-        elif phonetics.nameid == "MAU": # WebMAUS output
-            transform = None
-        else:
-            raise ValueError("Unexpected first tier found in file {:}: {:}".format(
-                file, phonetics.nameid))
 
-        windows_per_second = 1000 / p["frame_shift_ms"]
+        windows_per_second = 1000 / hparams["frame_shift_ms"]
 
         feature_matrix = numpy.zeros(
-            (spectrogram_length, N_FEATURES),
+            (spectrogram_length, len(SEGMENTS)),
             dtype=bool)
         for start, end, segment in phonetics.simple_transcript:
             start = float(start)
             end = float(end)
-            if transform:
-                segment = transform(segment)
-            window_features = feature_vector_of_sound(segment)
-            for window in range(int(start * windows_per_second),
-                                int(end * windows_per_second)):
-                try:
-                    feature_matrix[window] = window_features
-                except IndexError:
-                    continue
+            feature_matrix[
+                int(start * windows_per_second):int(end * windows_per_second),
+                SEGMENTS.index(segment)] = 1
         return feature_matrix
-
