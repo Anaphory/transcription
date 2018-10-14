6e5b12615911b88b95fab68b19f06353e97c5dcd
Gereon Kaiping <anaphory@yahoo.de>
2018-10-14 10:48:34 +0200

    Go back to the simpler model description

--- src/hparams.py
+++ src/hparams.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
+
+"""Model hyperparameters."""
+
 hparams = {
     "n_spectrogram": 513,
-    "n_lstm_hidden": 300,
+    "n_lstm_hidden": [300, 300, 300],
     "n_features_hidden": 40,
     "frame_length_ms": 15,
     "frame_shift_ms": 5,
